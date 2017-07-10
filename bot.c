0352eb282baa1a4d2d655a10d4ced353218ad522
Mark Furland <markfurland@gmail.com>
2017-07-10 15:29:10 -0400

    Use new config, fail out if can't read it all

--- bot.c
+++ bot.c
@@ -18,12 +18,6 @@ typedef struct config {
     char *channel;
 } config;
 
-char *host = "localhost";
-char *port = "6667";
-char *nick = "test";
-char *channel = "#test";
-config conf;
-
 static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
     if (tok->type == JSMN_STRING && (int) strlen(s) == tok->end - tok->start &&
             strncmp(json + tok->start, s, tok->end - tok->start) == 0) {
@@ -35,14 +29,13 @@ static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
 int saveToConfigIfMatch(char* name, char** configDest, char* configBuf, jsmntok_t* tokens, int token) {
     int length = tokens[token].end - tokens[token].start;
     if ((int)strlen(name) == length && strncmp(configBuf + tokens[token].start, name, length) == 0) {
-        printf("- %s: %.*s\n", name, tokens[token+1].end-tokens[token+1].start, configBuf + tokens[token+1].start);
         *configDest = strndup(configBuf + tokens[token+1].start, tokens[token+1].end-tokens[token+1].start);
         return 1;
     }
     return 0;
 }
 
-int readConfig(){
+int readConfig(config* conf){
     int r;
     int i;
     jsmn_parser p;
@@ -79,8 +72,6 @@ int readConfig(){
         printf("Failed to parse JSON: %d\n", r);
         return 1;
     }
-    printf("parsed\n");
-
 
     /* Assume the top-level element is an object */
     if (r < 1 || tokens[0].type != JSMN_OBJECT) {
@@ -90,19 +81,19 @@ int readConfig(){
 
     for (i = 1; i < r; i++) {
         if(tokens[i].type == JSMN_STRING || tokens[i].type == JSMN_PRIMITIVE) {
-            if (saveToConfigIfMatch("host", &conf.host, configBuf, tokens, i)) {
+            if (saveToConfigIfMatch("host", &(conf->host), configBuf, tokens, i)) {
                 i++;
                 continue;
             }
-            if (saveToConfigIfMatch("port", &conf.port, configBuf, tokens, i)) {
+            if (saveToConfigIfMatch("port", &(conf->port), configBuf, tokens, i)) {
                 i++;
                 continue;
             }
-            if (saveToConfigIfMatch("nick", &conf.nick, configBuf, tokens, i)) {
+            if (saveToConfigIfMatch("nick", &(conf->nick), configBuf, tokens, i)) {
                 i++;
                 continue;
             }
-            if (saveToConfigIfMatch("channel", &conf.channel, configBuf, tokens, i)) {
+            if (saveToConfigIfMatch("channel", &(conf->channel), configBuf, tokens, i)) {
                 i++;
                 continue;
             }
@@ -111,6 +102,15 @@ int readConfig(){
                     configBuf + tokens[i].start);
         }
     }
+    printf("*host: %s\n", conf->host);
+    printf("*port: %s\n", conf->port);
+    printf("*nick: %s\n", conf->nick);
+    printf("*channel: %s\n", conf->channel);
+    if(!conf->host || !conf->port || !conf->nick || !conf->channel) {
+        printf("didn't get full config\n");
+        return 1;
+    }
+    return 0;
 }
 void raw(char *fmt, ...) {
     va_list ap;
@@ -126,13 +126,13 @@ void intHandler(int dummy) {
     exit(0);
 }
 
-void processMessage(char* from, char* where, char* target, char* message){
-    if(!strncmp(message, nick, strlen(nick)) && message[strlen(nick)] == ':') {
+void processMessage(config conf, char* from, char* where, char* target, char* message){
+    if(!strncmp(message, conf.nick, strlen(conf.nick)) && message[strlen(conf.nick)] == ':') {
         char* action = NULL;
         char* args = NULL;
         //Addressed to us (in format 'nick: some message'
         //strip out our name
-        message += strlen(nick)+1;
+        message += strlen(conf.nick)+1;
         //There might be a space after :
         if(message[0] == ' ') {
             message++;
@@ -162,24 +162,24 @@ int main() {
     char buf[513];
     struct addrinfo hints, *res;
 
-    readConfig();
-    printf("*host: %s\n", conf.host);
-    printf("*port: %s\n", conf.port);
-    printf("*nick: %s\n", conf.nick);
-    printf("*channel: %s\n", conf.channel);
-    exit(0);
+    config conf;
+
+    if(readConfig(&conf) != 0) {
+        printf("can't read config, failing out");
+        exit(0);
+    }
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
-    getaddrinfo(host, port, &hints, &res);
+    getaddrinfo(conf.host, conf.port, &hints, &res);
     conn = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
     connect(conn, res->ai_addr, res->ai_addrlen);
 
     signal(SIGINT, intHandler);
 
-    raw("USER %s 0 0 :%s\r\n", nick, nick);
-    raw("NICK %s\r\n", nick);
+    raw("USER %s 0 0 :%s\r\n", conf.nick, conf.nick);
+    raw("NICK %s\r\n", conf.nick);
 
     while ((sl = read(conn, sbuf, 512))) {
         for (i = 0; i < sl; i++) {
@@ -222,8 +222,8 @@ int main() {
 
                     if (wordcount < 2) continue;
 
-                    if (!strncmp(command, "001", 3) && channel != NULL) {
-                        raw("JOIN %s\r\n", channel);
+                    if (!strncmp(command, "001", 3) && conf.channel != NULL) {
+                        raw("JOIN %s\r\n", conf.channel);
                     } else if (!strncmp(command, "PRIVMSG", 7) ) {
                         if (where == NULL || message == NULL) {
                             printf("where or message null");
@@ -244,7 +244,7 @@ int main() {
                             message[strlen(message)-2] = '\0';
                         }
                         printf("[from: %s] [where: %s] [reply-to: %s] %s\n", user, where, target, message);
-                        processMessage(user, where, target, message);
+                        processMessage(conf, user, where, target, message);
                     }
                 }//else the message isn't PING, and has no prefix. Not sure messages like this exist in the wild
             }//end if we have entire thing in buffer
